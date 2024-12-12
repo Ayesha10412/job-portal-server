bd8435c344636fbe3c2a6a6120a6f431c49b86b8
Your Name <k.bristi057@gmail.com>
2024-12-13 04:06:22 +0600

    added the post method for add job

--- index.js
+++ index.js
@@ -48,6 +48,12 @@ async function run() {
       res.send(result);
     });
 
+    app.post("/jobs", async (req, res) => {
+      const newJob = req.body;
+      const result = await jobsCollection.insertOne(newJob);
+      res.send(result);
+    });
+
     // job application api
     app.get("/job-application", async (req, res) => {
       const email = req.query.email;
